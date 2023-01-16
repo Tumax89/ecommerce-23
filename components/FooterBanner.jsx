de8f10533fb8a92530c23e4439aa77b2aa9694dd
Duger Tulga <t1420461925@gmail.com>
2023-01-16 21:45:46 +0800

    layout, header,footer хэсгийн зарим зүйлсийг хийв.

--- components/FooterBanner.jsx
+++ components/FooterBanner.jsx
@@ -1,10 +1,22 @@
+import React from "react";
+import Link from "next/link";
 
-import React from 'react';
-import Link from 'next/link';
+import { urlFor } from "../lib/client";
 
-import { urlFor } from '../lib/client';
-
-const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTime, smallText, midText, desc, product, buttonText, image } }) => {
+const FooterBanner = ({
+  footerBanner: {
+    discount,
+    largeText1,
+    largeText2,
+    saleTime,
+    smallText,
+    midText,
+    desc,
+    product,
+    buttonText,
+    image,
+  },
+}) => {
   return (
     <div className="footer-banner-container">
       <div className="banner-desc">
@@ -22,13 +34,10 @@ const FooterBanner = ({ footerBanner: { discount, largeText1, largeText2, saleTi
             <button type="button">{buttonText}</button>
           </Link>
         </div>
-
-        <img 
-          src={urlFor(image)} className="footer-banner-image"
-        />
+        <img src={urlFor(image)} className="footer-banner-image" />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FooterBanner
+export default FooterBanner;
