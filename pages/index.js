b9f418ad27d09adb04b9aebca89e51e540316299
Duger Tulga <dugertulga@Tulga.local>
2023-01-16 15:02:10 +0800

    herobanner хэсгийг sanity-тай холбов.

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,18 @@ import React from "react";
 import { client } from "../lib/client";
 import { Product, FooterBanner, HeroBanner } from "../components";
 
-const Home = ({}) => (
+const Home = ({ products, bannerData }) => (
   <div>
-    <HeroBanner />
+    <HeroBanner heroBanner={bannerData.length && bannerData[0]} />
+
     <div className="products-heading">
       <h2>Best Seller Products</h2>
       <p>speaker There are many variations passages</p>
     </div>
 
-    <div className="products-container">Products</div>
+    <div className="products-container">
+      {products?.map((product) => product.name)}
+    </div>
 
     <FooterBanner />
   </div>
